96b976570f378fcdbc11aaed44f3ac4dd6865973
Malik <mccolmalik@gmail.com>
2022-06-02 20:05:17 -0500

    Capstone

--- index.js
+++ index.js
@@ -16,3 +16,65 @@ function render(state = store.Home) {
   ${Main(state)}
   ${Footer()}
   `;
+  router.updatePageLinks();
+  afterRender();
+}
+
+function afterRender() {
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".navigation").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
+router.hooks({
+  before: (done, params) => {
+    let view = "Home";
+    if (params && params.data && params.data.view) {
+      view = capitalize(params.data.view);
+    }
+    if (view === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
+        )
+        .then(response => {
+          const kelvinToFahrenheit = kelvinTemp =>
+            Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+          store.Home.weather = {};
+          store.Home.weather.city = response.data.name;
+          store.Home.weather.temp = kelvinToFahrenheit(response.data.main.temp);
+          store.Home.weather.feelsLike = kelvinToFahrenheit(
+            response.data.main.feels_like
+          );
+          store.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => {
+          console.log(err);
+          done();
+        });
+    } else if (view === "Chat") {
+      axios
+        .get(`${process.env._API_URL}`)
+        .then(response => {
+          store.Chat.Room_id = response.data;
+          done();
+        })
+        .catch(error => {
+          console.log("It puked", error);
+          done();
+        });
+    } else {
+      done();
+    }
+  }
+});
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      render(store[view]);
+    }
+  })
+  .resolve();
