53f77b2be370e4096eb50efa25a269a6d7e6193b
Malik <mccolmalik@gmail.com>
2022-06-05 14:08:02 -0500

    Capstone

--- store/index.js
+++ store/index.js
@@ -2,5 +2,5 @@ export { default as Mario } from "./Mario";
 export { default as Yugioh } from "./Yugioh";
 export { default as Forhonor } from "./Forhonor";
 export { default as Home } from "./Home";
-export { default as Links } from "./Nlinks";
+export { default as Links } from "./links";
 export { default as About } from "./About";
