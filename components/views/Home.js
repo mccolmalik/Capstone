ee76cb4b3583a8c1123280640ce13d0ea2381bd4
Malik <mccolmalik@gmail.com>
2022-06-23 15:49:16 -0500

    Capstone

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,7 +1,11 @@
 import html from "html-literal";
-export default () => html`
+export default state => html`
   <p>
     Welcome to Donkey talk. The site or app where you can see your frustrations
     for select games expressed. This is a place of laughs so please relax.
   </p>
+  <h1>
+    Weather in ${state.weather.city} ${state.weather.temp}F, feels like
+    ${state.weather.feelsLike}
+  </h1>
 `;
